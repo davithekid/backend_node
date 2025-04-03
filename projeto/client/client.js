9d15e829a0c56d2b7eda638a387bf54f4852fae0
davithkid <byjdrk.27@gmail.com>
2025-04-03 10:10:38 -0300

    fix adm

--- projeto/client/client.js
+++ projeto/client/client.js
@@ -39,7 +39,6 @@ fs.readFile('../filmes.json', 'utf8', (err, data) => {
         }
     }
 
-
     async function exibirMenu() {
         console.log('\n');
         const perguntas = [
@@ -50,178 +49,210 @@ fs.readFile('../filmes.json', 'utf8', (err, data) => {
                 choices: [
                     { name: chalk.cyanBright('Exibir Catálogo'), value: 'listar' },
                     { name: chalk.cyanBright('Exibir detalhes do filme'), value: 'exibir' },
-                    { name: chalk.cyanBright('Adicionar um filme'), value: 'adicionar' },
-                    { name: chalk.cyanBright('Atualizar um filme'), value: 'atualizar' },
-                    { name: chalk.cyanBright('Deletar um filme'), value: 'deletar' },
+                    { name: chalk.cyanBright('Modo adm'), value: 'admin' },
                     { name: chalk.cyanBright('Sair da operação'), value: 'sair' }
                 ]
             }
         ];
 
-        try {
-            const resposta = await inquirer.prompt(perguntas)
-
-            switch (resposta.opcao) {
-                case 'listar':
-                    const filmes = await listarFilmes();
-
-                    if (Array.isArray(filmes) && filmes.length > 0) {
-                        console.log(chalk.green('Catálogo'))
-
-                        filmes.forEach(filme => {
-                            console.log(`${chalk.cyan(filme.id)}: ${chalk.blueBright(filme.nome)} , ${chalk.yellow(filme.genero)}`)
-                        })
-                    } else {
-                        console.log(chalk.yellow('Nenhum filme encontrado.'))
-                    }
-
-                    exibirMenu();
-                    break;
-
-                    case 'adicionar':
-
-                    const filmeNovo = await inquirer.prompt([
-                        {
-                            message: "Digite o ID do filme: ",
-                            type: "input",
-                            name: "id",
-                        },
-                        {
-                            message: "Digite o nome do filme: ",
-                            type: "input",
-                            name: "nome",
-                        },
-                        {
-                            message: "Digite o genero do filme: ",
-                            type: "input",
-                            name: "genero"
-
-                        },
-
-                    ]);
-
-                    try {
-
-                        filmeNovo.id = parseInt(filmeNovo.id)
-                        dados.push(dados.id = filmeNovo)
-
-                        const jsonData = JSON.stringify(dados, null, 2)
-
-                        console.log(chalk.green('Filme adicionado com sucesso!!'));
-                        fs.writeFileSync('../filmes.json', jsonData, err => {
-                            if (err) throw err;
-                        })
-
-                        axios.post('http://localhost:3000/filmes/', {
-                            id: filmeNovo.id,
-                            nome: filmeNovo.nome,
-                            genero: filmeNovo.genero
-                        })
-
-                    } catch (error) {
-                        console.error(chalk.red(error))
-                    }
-                    exibirMenu();
-                    break;
-                case 'atualizar':
-                    const filmeAtualizado = await inquirer.prompt([
-                        {
-                            message: "Digite o ID do filme: ",
-                            type: "input",
-                            name: "id",
-                        },
-                        {
-                            message: "Digite o novo nome do filme: ",
-                            type: "input",
-                            name: "nome",
-                        },
-                        {
-                            message: "Digite o genero do filme: ",
-                            type: "input",
-                            name: "genero"
-
-                        },
-
-                    ]);
-
-                    try {
-                        filmeAtualizado.id = parseInt(filmeAtualizado.id)
-
-                        dados[filmeAtualizado.id - 1] = filmeAtualizado
-                        const jsonData = JSON.stringify(dados, null, 2)
-
-                        console.log(chalk.green('Filme atualizado com sucesso!!'));
-                        fs.writeFileSync('../filmes.json', jsonData, err => {
-                            if (err) throw err;
-                        })
-
-                        axios.patch(`http://localhost:3000/filmes/${dados}`, {
-                            id: filmeAtualizado.id,
-                            nome: filmeAtualizado.nome,
-                            genero: filmeAtualizado.genero
-                            
-                        })
-                    } catch (error) {
-                        console.error(chalk.red(error))
-                    }
-                    exibirMenu();
-                    break;
-
-                case 'deletar':
-                    const deletarFilme = await inquirer.prompt([
-                        {
-                            message: "Digite o ID do filme: ",
-                            type: "input",
-                            name: "id",
-                        },
-
-                    ]);
-
-                    try {
-                        deletarFilme.id = parseInt(deletarFilme.id)
-                        dados.splice(dados.id = deletarFilme.id - 1, 1)
-                        const jsonData = JSON.stringify(dados, null, 2)
-
-                        console.log(chalk.green('Filme deletado com sucesso!!'));
-                        fs.writeFileSync('../filmes.json', jsonData, err => {
-                            if (err) throw err;
-                        })
-
-                        axios.delete(`http://localhost:3000/filmes/${dados.id}`)
+        async function exibirMenuADM() {
+            console.log('\n');
+            const menuADM = [
+                {
+                    type: "list",
+                    name: "opcaoADM",
+                    message: chalk.yellow('Escolha uma opção: '),
+                    choices: [
+                        { name: chalk.cyanBright('Exibir Catálogo'), value: 'listar' },
+                        { name: chalk.cyanBright('Exibir detalhes do filme'), value: 'exibir' },
+                        { name: chalk.cyanBright('Modo adm'), value: 'admin' },
+                        { name: chalk.cyanBright('Adicionar um filme'), value: 'adicionar' },
+                        { name: chalk.cyanBright('Atualizar um filme'), value: 'atualizar' },
+                        { name: chalk.cyanBright('Deletar um filme'), value: 'deletar' },
+                        { name: chalk.cyanBright('Sair da operação'), value: 'sair' }
+                    ]
+                }
+            ];
 
 
-                    } catch (error) {
-                        console.error(chalk.red(error))
-                    }
-                    exibirMenu();
-                    break;
+            try {
+                const resposta = await inquirer.prompt(perguntas)
 
-                case 'exibir':
-                    const idReposta = await inquirer.prompt([
-                        {
-                            type: "input",
-                            name: "id",
-                            message: chalk.blue('Digite o ID do filme: ')
+                switch (resposta.opcao) {
+                    case 'listar':
+                        const filmes = await listarFilmes();
+
+                        if (Array.isArray(filmes) && filmes.length > 0) {
+                            console.log(chalk.green('Catálogo'))
+
+                            filmes.forEach(filme => {
+                                console.log(`${chalk.cyan(filme.id)}: ${chalk.blueBright(filme.nome)} , ${chalk.yellow(filme.genero)}`)
+                            })
+                        } else {
+                            console.log(chalk.yellow('Nenhum filme encontrado.'))
                         }
-                    ]);
 
-                    const filme = await exibirDetalhesCatálogo(idReposta.id);
-                    if (filme) {
-                        console.log(`${chalk.cyan(filme.id)}: ${chalk.blueBright(filme.nome)} ${chalk.yellow(filme.genero)}`)
-                    } else {
-                        console.log(chalk.yellow('Filme não encontrado'))
-                    }
-                    exibirMenu();
-                    break;
+                        exibirMenu();
+                        break;
 
-                case 'sair':
-                    console.log(chalk.yellow('Saindo do sistema...'))
-                    break;
+                    case 'admin':
+                        const admin = await inquirer.prompt([
+                            { message: 'Digite a senha: ', type: 'password', name: 'senha' }
+                        ])
+
+                        if (admin.senha === 'SEGREDO') {
+
+                            const respostaADM = await inquirer.prompt(menuADM)
+
+                            switch (respostaADM.opcaoADM) {
+                                case 'adicionar':
+
+                                    const filmeNovo = await inquirer.prompt([
+                                        {
+                                            message: "Digite o ID do filme: ",
+                                            type: "input",
+                                            name: "id",
+                                        },
+                                        {
+                                            message: "Digite o nome do filme: ",
+                                            type: "input",
+                                            name: "nome",
+                                        },
+                                        {
+                                            message: "Digite o genero do filme: ",
+                                            type: "input",
+                                            name: "genero"
+
+                                        },
+
+                                    ]);
+
+                                    try {
+
+                                        filmeNovo.id = parseInt(filmeNovo.id)
+                                        dados.push(dados.id = filmeNovo)
+
+                                        const jsonData = JSON.stringify(dados, null, 2)
+
+                                        console.log(chalk.green('Filme adicionado com sucesso!!'));
+                                        fs.writeFileSync('../filmes.json', jsonData, err => {
+                                            if (err) throw err;
+                                        })
+
+                                        axios.post('http://localhost:3000/filmes/', {
+                                            id: filmeNovo.id,
+                                            nome: filmeNovo.nome,
+                                            genero: filmeNovo.genero
+                                        })
+
+                                    } catch (error) {
+                                        console.error(chalk.red(error))
+                                    }
+                                    exibirMenu();
+                                    break;
+                                case 'atualizar':
+                                    const filmeAtualizado = await inquirer.prompt([
+                                        {
+                                            message: "Digite o ID do filme: ",
+                                            type: "input",
+                                            name: "id",
+                                        },
+                                        {
+                                            message: "Digite o novo nome do filme: ",
+                                            type: "input",
+                                            name: "nome",
+                                        },
+                                        {
+                                            message: "Digite o genero do filme: ",
+                                            type: "input",
+                                            name: "genero"
+
+                                        },
+
+                                    ]);
+
+                                    try {
+                                        filmeAtualizado.id = parseInt(filmeAtualizado.id)
+
+                                        dados[filmeAtualizado.id - 1] = filmeAtualizado
+                                        const jsonData = JSON.stringify(dados, null, 2)
+
+                                        console.log(chalk.green('Filme atualizado com sucesso!!'));
+                                        fs.writeFileSync('../filmes.json', jsonData, err => {
+                                            if (err) throw err;
+                                        })
+
+                                        axios.patch(`http://localhost:3000/filmes/${dados}`, {
+                                            id: filmeAtualizado.id,
+                                            nome: filmeAtualizado.nome,
+                                            genero: filmeAtualizado.genero
+
+                                        })
+                                    } catch (error) {
+                                        console.error(chalk.red(error))
+                                    }
+                                    exibirMenu();
+                                    break;
+
+                                case 'deletar':
+                                    const deletarFilme = await inquirer.prompt([
+                                        {
+                                            message: "Digite o ID do filme: ",
+                                            type: "input",
+                                            name: "id",
+                                        },
+
+                                    ]);
+
+                                    try {
+                                        deletarFilme.id = parseInt(deletarFilme.id)
+                                        dados.splice(dados.id = deletarFilme.id - 1, 1)
+                                        const jsonData = JSON.stringify(dados, null, 2)
+
+                                        console.log(chalk.green('Filme deletado com sucesso!!'));
+                                        fs.writeFileSync('../filmes.json', jsonData, err => {
+                                            if (err) throw err;
+                                        })
+
+                                        axios.delete(`http://localhost:3000/filmes/${dados.id}`)
+
+
+                                    } catch (error) {
+                                        console.error(chalk.red(error))
+                                    }
+                                    exibirMenu();
+                                    break;
+                            }
+
+                        }
+                        break;
+                    case 'exibir':
+                        const idReposta = await inquirer.prompt([
+                            {
+                                type: "input",
+                                name: "id",
+                                message: chalk.blue('Digite o ID do filme: ')
+                            }
+                        ]);
+
+                        const filme = await exibirDetalhesCatálogo(idReposta.id);
+                        if (filme) {
+                            console.log(`${chalk.cyan(filme.id)}: ${chalk.blueBright(filme.nome)} ${chalk.yellow(filme.genero)}`)
+                        } else {
+                            console.log(chalk.yellow('Filme não encontrado'))
+                        }
+                        exibirMenu();
+                        break;
+
+                    case 'sair':
+                        console.log(chalk.yellow('Saindo do sistema...'))
+                        break;
+                }
+            } catch (error) {
+                console.error('erro', error)
             }
-        } catch (error) {
-            console.error('erro', error)
         }
-
+        exibirMenuADM();
     };
     exibirMenu();
 });
